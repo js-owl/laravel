c8cebaf5166fcf6df2e6a4f80578c57b8b3b6e85
Alex K <aklimovskikh@yandex.ru>
2024-08-18 15:28:26 +0300

    5 lesson: end

---
+++ app/Enums/Cars/Status.php
@@ -0,0 +1,22 @@
+<?php
+
+// namespace App\Enums\Cars;
+
+// enum Status{
+//     case DRAFT = 0;
+//     case ACTIVE = 5;
+//     case SOLD = 10;
+//     case CANCELED = 15;
+//     public function text(){
+//         return match($this){
+//             self::DRAFT => 'Черновик',
+//             self::ACTIVE => 'Активно',
+//             self::SOLD => 'Продано',
+//             self::CANCELED => 'ОТменено',
+//         }
+//     }
+// }
+
+// class CarStatuses{
+//     public static $draft = 0;
+// }
